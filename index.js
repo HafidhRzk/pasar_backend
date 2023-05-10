3feeb281492dac16fa535aabdf036d4fd846a780
HafidhRizky <hafidhrzk@gmail.com>
2023-05-10 14:36:33 +0700

    push

--- index.js
+++ index.js
@@ -1,53 +1,17 @@
 const express = require("express");
-const helmet = require('helmet');
+require("dotenv").config();
+// const router = require("./src/routes");
 const cors = require("cors");
-const swaggerUI = require("swagger-ui-express");
-
-const router = require("./routes/index");
-const swaggerDocument = require("./swagger.json");
-
-const errorHandler = require("./middlewares/errorHandler");
-
-const port = process.env.PORT || 3006;
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-exports.appRoot = () => {
-  let path = require("path").resolve(__dirname);
-  return path;
-};
-
-app.use(helmet({
-  crossOriginEmbedderPolicy: false,
-  crossOriginResourcePolicy: false
-}));
 app.use(cors());
+app.use(express.json());
+// app.use("/api/v1", router);
 
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-app.use(express.json({ limit: "5mb" }));
-
-app.use(express.static("public"));
-
-app.use(
-  "/api-docs",
-  function (req, res, next) {
-    swaggerDocument.host = req.get("host");
-    req.swaggerDoc = swaggerDocument;
-    next();
-  },
-  swaggerUI.serve,
-  swaggerUI.setup(swaggerDocument)
-);
-app.use("/", router);
-
-app.all("*", (req, res, next) => {
-  next({ statusCode: 404, message: "Endpoint not found" });
+app.get("/", (req, res) => {
+  res.send("Hello Developer!");
 });
 
-app.use(errorHandler);
-
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => console.log(`Server running on ${port}`));
-}
-
-module.exports = app;
+app.listen(port, () => {console.log(`Server is listening on port ${port}`);});
